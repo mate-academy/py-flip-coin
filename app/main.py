1a09cf70415b2a5dd56b6ab866daea473d1fad35
Odin-max <meh84741@gmail.com>
2025-02-12 18:32:36 +0200

    2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import random
-import matplotlib.pyplot as plt
 from collections import Counter
 
- 
-def flip_coin(trials: int = 10000, flips_per_trial:int = 10) -> dict:
-    results = [sum(random.choice([0, 1]) for _ in range(flips_per_trial)) for _ in range(trials)]
+
+def flip_coin(trials: int = 10000, flips_per_trial:int = 10) -> dict: 
+    results = [
+        sum(random.choice(
+            [0, 1]
+        ) for _ in range(flips_per_trial)) for _ in range(trials)
+    ]
     counts = Counter(results)
     total = sum(counts.values())
     return {k: round(v / total * 100, 2) for k, v in sorted(counts.items())}
