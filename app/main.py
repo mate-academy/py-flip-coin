0b3ad5404e75683a6a2099373535790dc71054dd
Yaroslav <yaroslav.liubarets@icloud.com>
2025-02-13 12:44:17 +0000

    Solution 1.1

--- app/main.py
+++ app/main.py
@@ -1,47 +1,14 @@
 import random
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 def flip_coin() -> dict:
-    result_dict = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0,
-    }
-    choices = ("heads", "tails")
+    trials = 10000
+    heads_count = {i: 0 for i in range(11)}
 
-    for _ in range(10000):
-        heads_count = 0
-        for _ in range(10):
-            choice = random.choice(choices)
-            if choice == "heads":
-                heads_count += 1
-        result_dict[heads_count] += 0.01
-        result_dict[heads_count] = round(result_dict[heads_count], 2)
+    for _ in range(trials):
+        heads = sum(random.choice([0, 1]) for _ in range(10))
+        heads_count[heads] += 1
 
-    return result_dict
+    heads_percentage = {k: (v / trials) * 100 for k, v in heads_count.items()}
 
-
-def draw_gaussian_distribution_graph() -> None:
-    flips_dict = flip_coin()
-
-    x_points = np.array(list(flips_dict.keys()))
-    y_points = np.array(list(flips_dict.values()))
-
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-
-    plt.plot(x_points, y_points)
-    plt.show()
-
-
-draw_gaussian_distribution_graph()
+    return heads_percentage
