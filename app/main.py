e24f8b0521033aaf526e7ead9ad90b20a52f6a16
MTCH <dimamitich2@gmail.com>
2025-02-17 15:41:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,22 +15,3 @@ def flip_coin(num_experiments: int = 10000, num_flips: int = 10) -> dict:
     }
 
     return {i: percentages.get(i, 0) for i in range(num_flips + 1)}
-
-
-def draw_gaussian_distribution_graph(data: dict) -> None:
-    plt.figure(figsize=(10, 6))
-
-    heads = list(data.keys())
-    percentages = list(data.values())
-
-    plt.plot(heads, percentages, "b-")
-
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.title("Gaussian distribution")
-
-    plt.xlim(0, 10)
-    plt.ylim(0, 100)
-
-    plt.grid(True)
-    plt.show()
