be6a0fc90064e72163945eb261a34b57ff04ccb0
Mortredas <mortreda13@gmail.com>
2025-02-20 17:25:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from typing import Dict
+
+import random
+
+
+def flip_coin() -> Dict[int, float]:
+    count = [0] * 11
+    trials = 10000
+
+    for _ in range(trials):
+        heads = sum(random.choice([0, 1]) for _ in range(10))
+        count[heads] += 1
+
+    frequencies = list((i / trials) * 100 for i in count)
+
+    return dict(zip(range(11), frequencies))
+
+
+results = flip_coin()
+for head_count, frequency in results.items():
+    print(f"{head_count} heads: {frequency}%")
