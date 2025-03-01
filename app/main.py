9337b5df08fcc612c545b392b193071bdd17a311
Pavlo <vojtovp20@gmail.com>
2025-03-01 11:51:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin(times: int = 10000) -> dict:
+    random_coin = {}
+
+    for _ in range(times):
+        value = sum([random.choice([0, 1]) for _ in range(10)])
+        random_coin[value] = random_coin.get(value, 0) + 1
+
+    random_coin = {k: round((v / times) * 100, 2)
+                   for k, v in sorted(random_coin.items())}
+    return random_coin
