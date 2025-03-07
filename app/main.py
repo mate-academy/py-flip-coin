926183c8b2e77f5482ab9e256392fdbeedb5aeb5
AndyStar <andy_star@ukr.net>
2025-03-07 21:34:25 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import random
+import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -15,5 +17,25 @@ def flip_coin() -> dict:
             count_all[count_head] = 1
         else:
             count_all[count_head] = count_head_stat + 1
-    return {heads: round(counter / cases * 100, 2)
-            for heads, counter in count_all.items()}
+
+    # xpoints = np.array([1, 8])
+    # ypoints = np.array([3, 10])
+
+    result = {heads: round(counter / cases * 100, 2)
+              for heads, counter in count_all.items()}
+
+    result_sorted = OrderedDict(sorted(result.items()))
+
+    return result_sorted
+
+
+result_sorted = flip_coin()
+x_points = [element for element in result_sorted.keys()]
+y_points = [element for element in result_sorted.values()]
+
+plt.title("Gaussian distribution")
+plt.xlabel("Heads count")
+plt.ylabel("Drop percentage %")
+
+plt.plot(x_points, y_points)
+plt.show()
