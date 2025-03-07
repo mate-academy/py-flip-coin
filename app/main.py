4d538d7ad4cfd9ae1aadd1b05f6cef3af25b1ffc
AndyStar <andy_star@ukr.net>
2025-03-07 20:18:05 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    count_all = dict()
+    cases = 20000
+    flips = 10
+    for _ in range(cases):
+        count_head = 0
+        for _ in range(flips):
+            if random.randint(0, 1) == 0:
+                count_head += 1
+        count_head_stat = count_all.get(count_head)
+        if count_head_stat is None:
+            count_all[count_head] = 1
+        else:
+            count_all[count_head] = count_head_stat + 1
+    return {heads: round(counter / cases * 100, 2)
+            for heads, counter in count_all.items()}
