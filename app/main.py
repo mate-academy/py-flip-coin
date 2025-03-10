e1f8401b3d8c3c2e90f3b97ba8a8ea43528eb31b
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-03-10 13:16:44 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def one_case() -> list:
+    return [random.randint(0, 1) for _ in range(10)].count(1)
+
+
+def flip_coin() -> dict:
+    all_casses = [one_case() for _ in range(10_000)]
+    how_much = [all_casses.count(x) for x in range(11)]
+    return {key: (how_much[key] / 100) for key in range(11)}
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    xpoints = np.array(list(data.keys()))
+    ypoints = np.array(list(data.values()))
+    plt.plot(xpoints, ypoints)
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.title("Gaussian distribution")
+    plt.show()
