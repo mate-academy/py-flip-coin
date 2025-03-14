52f9a43fc613a8d79101df65955b8d1ae137b804
Volodymyr Vitsenko <your@mail.com>
2025-03-14 20:30:15 +0100

    gaussian distribution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 import random
 
-import matplotlib.pyplot as plt
-
 
 def flip_coin() -> dict:
     result_dict = {num: 0.0 for num in range(0, 11)}
@@ -10,16 +8,3 @@ def flip_coin() -> dict:
     for key, value in result_dict.items():
         result_dict[key] = round((value / 10000) * 100, 2)
     return result_dict
-
-
-def draw_gaussian_distribution_graph(dictionary: dict) -> None:
-    plt.plot(
-        dictionary.keys(),
-        dictionary.values()
-    )
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.show()
-
-
-draw_gaussian_distribution_graph(flip_coin())
