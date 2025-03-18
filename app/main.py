75487e93179dfe3425acf34333d7de2ca572932d
Zahar <galushkozahar>
2025-03-18 20:34:36 +0200

    Run flake8

--- app/main.py
+++ app/main.py
@@ -1,23 +1,33 @@
 import random
-
 import matplotlib.pyplot as plt
 
-from collections import Counter
+
+def flip_coin() -> dict:
+    result = {key: 0 for key in range(11)}
+    for _ in range(10000):
+        count_head = 0
+        for i in range(10):
+            flip = random.choice(["head", "tail"])
+            if flip == "head":
+                count_head += 1
+        result[count_head] += 1
+
+    for key, value in result.items():
+        result[key] = round((value / 10000) * 100, 2)
+    return result
 
 
-def flip_coin(trials: int =10000, flips_per_trial: int = 10) -> None:
-    results = [sum(random.choice([0, 1]) for i in range(trials))
-               for i in range(flips_per_trial)]
-    count = Counter(results)
-    total_trials = sum(count.values())
-    return {k: round(v / total_trials * 100, 2)
-            for k, v in sorted(count.items())}
+data = flip_coin()
 
 
-def draw_gaussian_distribution_graph(data):
-    plt.bar(data.keys(), data.values(), color='blue', alpha=0.7)
-    plt.xlabel("Number of Heads in 10 Coin Flips")
-    plt.ylabel("Percentage of Occurrences (%)")
-    plt.title("Gaussian Distribution for 10 Coin Flips")
-    plt.grid(axis='y', linestely='--', alpha=0.7)
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    x_ = list(data.keys())
+    y_ = list(data.values())
+    plt.figure(figsize=(8, 5))
+    plt.plot(x_, y_, marker="o", linestyle="-", color="blue")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.ylabel("Drop percentage %")
+    plt.ylim(0, 100)
+    plt.ylim(0, 100)
     plt.show()
