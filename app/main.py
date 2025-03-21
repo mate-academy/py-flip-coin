15221eba62d20f95747a93d5374e14a04f93101b
Max <smolinskijmaksim1@gmail.com>
2025-03-21 11:55:39 +0100

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import random
 import matplotlib.pyplot as plt
 
 
-def flip_coin():
+def flip_coin() -> dict:
     results = {i: 0 for i in range(11)}
 
-    for _ in range(10000):  # 10 000 спроб
+    for _ in range(10000):
         count_heads = sum(random.randint(0, 1) for _ in range(10))
         results[count_heads] += 1
 
@@ -15,13 +15,13 @@ def flip_coin():
     return results
 
 
-def draw_gaussian_distribution_graph(results: dict):
+def draw_gaussian_distribution_graph(results: dict) -> None:
     plt.plot(
         list(results.keys()),
         list(results.values()),
-        marker='o',
-        linestyle='-',
-        color='blue'
+        marker="o",
+        linestyle="-",
+        color="blue"
     )
 
     plt.xlabel("Number of Heads")
@@ -29,6 +29,6 @@ def draw_gaussian_distribution_graph(results: dict):
     plt.title("Distribution of Heads in 10 Coin Flips (10,000 trials)")
 
     plt.xticks(range(11))
-    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.grid(True, linestyle="--", alpha=0.7)
 
     plt.show()
