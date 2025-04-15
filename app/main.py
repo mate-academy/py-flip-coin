d98ac4b221fb377f6f6d2cb0bcdff702967bccb4
Michal-Naron <michanaron@gmail.com>
2025-04-16 00:36:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> None:
+    dictionary = {i: 0 for i in range(11)}
+    for _ in range(1000):
+        head = 0
+        for _ in range(10):
+            coin = random.choice(["head", "tail"])
+            if coin == "head":
+                head += 1
+        dictionary[head] += 1
+
+    dictionary2 = {key: round(value / 10, 2) for key, value in
+                   dictionary.items()}
+    print(dictionary2)
+    return dictionary2
