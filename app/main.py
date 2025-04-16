30de3d148dcf06ea367e5853927ed85dae2583fe
Michal-Naron <michanaron@gmail.com>
2025-04-16 15:28:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import random
 
 
-def flip_coin() -> None:
+def flip_coin() -> dict:
     dictionary = {i: 0 for i in range(11)}
-    for _ in range(1000):
+    for _ in range(10000):
         head = 0
         for _ in range(10):
             coin = random.choice(["head", "tail"])
@@ -11,7 +11,7 @@ def flip_coin() -> None:
                 head += 1
         dictionary[head] += 1
 
-    dictionary2 = {key: round(value / 10, 2) for key, value in
+    dictionary2 = {key: round(value / 100, 2) for key, value in
                    dictionary.items()}
-    print(dictionary2)
+
     return dictionary2
