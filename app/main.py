03cdeb007fa10aebc0165e0473ec5e0d687850b4
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-20 16:20:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ def flip_coin() -> dict[int, float]:
         result_dict[random.choices(["Avers", "Revers"]
                                    , k=10).count("Avers")] += 1
 
-    for key, val in result_dict.items():
-        result_dict[key] = round((val / flipping_cases) * 100, 2)
+    for key, value in result_dict.items():
+        result_dict[key] = round((value / flipping_cases) * 100, 2)
 
     return result_dict
 
 
-def draw_gaussian_distribution_graph(res_dict: dict[int, float]):
+def draw_gaussian_distribution_graph(res_dict: dict[int, float]) -> None:
     xpoint = np.array(list(res_dict.keys()))
     ypoint = np.array(list(res_dict.values()))
 
