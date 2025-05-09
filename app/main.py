5e242ff379d4650741a846d729e99366d3d40825
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2025-05-09 14:07:33 +0300

    Solution_4

--- app/main.py
+++ app/main.py
@@ -1,13 +1,36 @@
+import random
 import matplotlib.pyplot as plt
+from typing import Dict
 
 
-x = list(range(11))
-y = [0, 1, 4, 12, 20, 25, 20, 12, 4, 1, 0]
+def flip_coin(
+        trials: int = 10000, flips_per_trial: int = 10
+) -> Dict[int, float]:
+    results: Dict[int, int] = {i: 0 for i in range(flips_per_trial + 1)}
 
-plt.plot(x, y, color="blue")
+    for _ in range(trials):
+        heads_count = (
+            sum(random.choice([0, 1]) for _ in range(flips_per_trial))
+        )
+        results[heads_count] += 1
 
-plt.xlabel("Heads count")
-plt.ylabel("Drop percentage %")
-plt.title("Gaussian distribution")
+    # Конвертуємо в проценти
+    return {k: round((v / trials) * 100, 2) for k, v in results.items()}
 
-plt.show()
+
+def draw_gaussian_distribution_graph(data: Dict[int, float]) -> None:
+    head_counts = sorted(data.keys())
+    percentages = [data[count] for count in head_counts]
+
+    plt.plot(head_counts, percentages, color="blue")
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.ylim(0, 100)
+    plt.show()
+
+
+# Використання
+distribution = flip_coin()
+print(distribution)
+draw_gaussian_distribution_graph(distribution)
