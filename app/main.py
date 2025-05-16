738d8228bffa32e6775374d0cdd7a50097db19a7
Kostya Mileant <kostya.milya@gmail.com>
2025-05-16 13:41:21 +0200

    SOLUTION

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+from collections import defaultdict
+
+def flip_coin(trials=10000):
+    results = defaultdict(int)
+
+    for _ in range(trials):
+        heads = sum(random.choice([0, 1]) for _ in range(10))
+        results[heads] += 1
+
+
+    percentages = {i: round((results[i] / trials) * 100, 2) for i in range(11)}
+    return percentages
+print(flip_coin())
