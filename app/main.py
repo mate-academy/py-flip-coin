2940fabba2d613de89d3d9b337bf902e05240bd5
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 21:13:06 +0300

    Not yet

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import random
+from matplotlib import pyplot as plt
+
+
+def flip_coin() -> dict:
+    results = {amount: 0 for amount in range(11)}
+    coin = ["head", "tail"]
+
+    for _ in range(10000):
+        head = 0
+        for _ in range(10):
+            result = random.choices(coin)
+            if result == "head":
+                head += 1
+        results[head] += 1
+
+    statistics = {amount: round((results[amount] / 10000) * 100, 2)
+                  for amount in results}
+
+    x = list(statistics.keys())
+    y = list(statistics.values())
+    plt.plot(x, y)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.show()
+
+    return statistics
