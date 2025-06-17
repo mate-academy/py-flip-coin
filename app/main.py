b23c853f77ae692f038d4c4dce788013543f6a0f
kravetst <taraskravetc28@gmail.com>
2025-06-17 18:32:33 +0300

    created a chart based on a function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def flip_coin() -> dict:
+    result_dict = {}
+    count = 0
+    while count < 10001:
+        heads_count = 0
+        for _ in range(10):
+            if random.randint(0, 1) == 1:
+                heads_count += 1
+        result_dict[heads_count] = result_dict.get(heads_count, 0) + 1
+        count += 1
+
+    for key in result_dict:
+        result_dict[key] = round(result_dict[key] / 10001 * 100, 2)
+    return result_dict
+
+
+def draw_gaussian_distribution_graph() -> None:
+    data_func = flip_coin()
+    x = np.array(list(data_func.keys()))
+    y = np.array(list(data_func.values()))
+    plt.plot(x, y)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Percentage of cases (%)")
+
+    plt.show()
