71bd7a530bcc6517b2a3ce1e6f9d5923077f8d6e
Dmitrii Kozoriz <diluck2002@gmail.com>
2025-06-26 06:55:35 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import random
 
 
 coin_sides = ["Heads", "Tails"]
+
+
 def flip_coin(n_trials: int = 10000, n_flips: int = 10) -> dict:
     result_dict = {}
     for _ in range(n_trials):
@@ -13,15 +15,16 @@ def flip_coin(n_trials: int = 10000, n_flips: int = 10) -> dict:
                 counter += 1
         result_dict[counter] = result_dict.get(counter, 0) + 1
     for key, value in enumerate(result_dict):
-        result_dict[key] = round(result_dict[key]/n_trials * 100, 2)
+        result_dict[key] = round(result_dict[key] / n_trials * 100, 2)
     return dict(sorted(result_dict.items()))
 
-def draw_gaussian_distribution_graph(result_dict: dict) -> None:
-    x = list(result_dict.keys())
-    y = list(result_dict.values())
 
-    plt.plot(x, y)
-    plt.ylim(0,100)
+def draw_gaussian_distribution_graph(result_dict: dict) -> None:
+    x_l = list(result_dict.keys())
+    y_l = list(result_dict.values())
+
+    plt.plot(x_l, y_l)
+    plt.ylim(0, 100)
     plt.xlabel("Heads count")
     plt.ylabel("Drop percentage%")
     plt.title("Gaussian distribution")
