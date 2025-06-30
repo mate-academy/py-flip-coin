91f1010e4eb0b9c545f765742ad64c2e5ca81a69
Maksym Buslenko <maksbusl@gmail.com>
2025-07-01 00:48:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    dict_of_head_percentage = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    for i in range(10001):
+        head_percentage = 0
+        for j in range(10):
+            coin = ["head", "tail"]
+            flip = random.choice(coin)
+            if flip == "head":
+                head_percentage += 1
+        dict_of_head_percentage[head_percentage] += 1
+    for _ in range(11):
+        dict_of_head_percentage[_] = (dict_of_head_percentage[_] / 10000) * 100
+
+    return dict_of_head_percentage
+
+
+
+print(flip_coin())
