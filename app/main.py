4e43638e469e3e9a464920de4024de7b98cc6859
turulko-oleksandr <SashaTurulko@i.ua>
2025-06-30 14:22:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,21 +9,21 @@ def flip_coin() -> dict:
 
     for i in range(total_cases):
         flips = []
-        for j in range(0, 10):
+        for case in range(0, 10):
             flips.append(rnd.choice([0, 1]))
         heads_count = sum(flips)
         results[heads_count] += 1
 
-    for k in results:
-        results[k] = round((results[k] / total_cases) * 100, 2)
+    for result in results:
+        results[result] = round((results[result] / total_cases) * 100, 2)
 
     return results
 
 
 def draw_gaussian_distribution_graph(results: dict) -> None:
-    x = np.array(results.keys())
-    y = np.array(results.values())
-    plt.plot(x, y)
+    x_val = np.array(results.keys())
+    y_val = np.array(results.values())
+    plt.plot(x_val, y_val)
 
     plt.xlabel("Heads count")
     plt.ylabel("Drop percentage")
