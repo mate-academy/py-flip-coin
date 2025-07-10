16fa03d8d6c2aa5018a62d46381d9a1480333a8f
viannik <ivan.nikitski@gmail.com>
2025-07-10 20:20:43 +0300

    Add coin flip simulation and Gaussian distribution graph

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    flips = {i: 0 for i in range(11)}
+
+    for _ in range(10000):
+        heads = sum(random.choice([0, 1]) for _ in range(10))
+        flips[heads] += 1
+
+    for heads in flips:
+        flips[heads] = round((flips[heads] / 10000) * 100, 2)
+
+    return flips
+
+
+def draw_gaussian_distribution_graph(flips: dict) -> None:
+    import matplotlib.pyplot as plt
+
+    x_vals = list(flips.keys())
+    y_vals = list(flips.values())
+
+    plt.plot(x_vals, y_vals)
+    plt.xlabel("Heads Count")
+    plt.ylabel("Drop percentage %")
+    plt.title("Gaussian distribution")
+    plt.xticks(range(11))
+    plt.yticks(range(0, 101, 10))
+
+    plt.show()
