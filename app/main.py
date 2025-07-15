dc552b973d3ce0e3b7652c741b598db56f177419
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-15 17:33:17 +0300

    fix: flip coin

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import random
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
-def flip_coin(n_flips: int = 10, n_cases: int = 100_000):
+def flip_coin(n_flips: int = 10, n_cases: int = 100_000) -> dict:
     list_zero = []
     for _ in range(n_cases):
         count = 0
@@ -21,15 +21,12 @@ def flip_coin(n_flips: int = 10, n_cases: int = 100_000):
     return dict_result
 
 
-def draw_gaussian_distribution_graph(dict_result):
-    x = np.array(list(dict_result.keys()))
-    y = np.array(list(dict_result.values()))
-
-    plt.plot(x, y, marker='o')
-
-    plt.xlabel("Number of heads")
-    plt.ylabel("Drop percentage %")
-    plt.title("Gaussian distribution of coin flips")
-    plt.grid(True)
-    plt.show()
-
+# def draw_gaussian_distribution_graph(dict_result: dict) -> None: # Im try:)
+#     x = np.array(list(dict_result.keys()))
+#     y = np.array(list(dict_result.values()))
+#     plt.plot(x, y, marker='o')
+#     plt.xlabel("Number of heads")
+#     plt.ylabel("Drop percentage %")
+#     plt.title("Gaussian distribution of coin flips")
+#     plt.grid(True)
+#     plt.show()
