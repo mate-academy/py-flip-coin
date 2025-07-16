2b928787273ca8b66e154ee839dae72cc9a748f1
reinvvay <reinvvay@gmail.com>
2025-07-16 20:14:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result = {i: 0.0 for i in range(11)}
+
+    for _ in range(10000):
+        flips = [random.randint(0, 1) for _ in range(10)]
+        result[sum(flips)] += 1
+
+    for key in result:
+        result[key] = round(result[key] / 10000 * 100, 2)
+
+    return result
