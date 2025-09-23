b461274fe8f81fc6ecf8ff69937858e10f396bc1
AKupriichuk <alexa05kup@gmail.com>
2025-09-23 13:22:35 +0300

    Commit 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    table_statistic = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        head_count = 0
+        for _ in range(10):
+            result = random.randint(0, 1)
+            if result == 1:
+                head_count += 1
+
+        table_statistic[head_count] += 1
+
+    percentages = {}
+    for head_count, count in table_statistic.items():
+        percentage = (count / 10000) * 100
+        percentages[head_count] = percentage
+    return percentages
+
+
+def draw_gaussian_distribution_graph() -> None:
+    result = flip_coin()
+    list_x = list(result.keys())
+    list_y = list(result.values())
+    plt.bar(list_x, list_y)
+    plt.xlabel("Heads count")
+    plt.ylabel("Percentage")
+    plt.show()
+
+
+if __name__ == "__main__":
+    draw_gaussian_distribution_graph()
