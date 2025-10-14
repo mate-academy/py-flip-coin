531611c7b3ddc06ac3f12c2c684b24c4ac2a9559
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-10-14 14:57:10 +0200

    first

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import random
+
+
+
+def flip_coin():
+    coin = ["heads", "tails"]
+    counts = {k: 0 for k in range(11)}
+    result_dict = {}
+    total = 10000
+    for i in range(total):
+        heads_count = 0
+        for j in range(10):
+            random_choice = random.choice(coin)
+            if random_choice == "heads":
+                heads_count += 1
+        counts[heads_count] += 1
+
+    for k in range(11):
+        percentage = counts[k] / total * 100
+        rounded_percentage = round(percentage, 2)
+        result_dict[k] = rounded_percentage
+
+    print (result_dict)
+    return result_dict
+
+flip_coin()
