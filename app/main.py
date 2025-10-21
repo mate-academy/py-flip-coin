f58e719f871b8a458ffa77a9e6e4f751742acc08
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-21 14:11:06 +0200

    chance

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from random import choice
+
+
+def flip_coin(
+        trials: int = 10000,
+        flips_per_trial: int = 10
+) -> dict:
+    dic = {i: 0 for i in range(0, flips_per_trial + 1)}
+    for _ in range(trials):
+        count_of_heads = sum(choice([1, 0]) for _ in range(flips_per_trial))
+        dic[count_of_heads] += 1
+    for key in dic:
+        dic[key] = round(dic[key] / trials * 100, 2)
+    return dic
