c9d2dcb7def54c78562885c2a8bf35e9c7fb60b2
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-23 14:31:03 +0200

    implementing flip_coin

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 import random
 
-def flip_coin() -> None:
-    how_many = 0
-    coin = random.randint(0, 1)
-    if coin == 1:
-        how_many += 1
-    print(how_many)
+def flip_coin() -> dict[int, float]:
+    counts = {key: 0 for key in range(11)}
 
+    for i in range(10000):
+        how_many = 0
+        for _ in range(10):
+            coin = random.randint(0, 1)
+            if coin == 1:
+                how_many += 1
+        counts[how_many] += 1
+    result = {}
+    for key, value in counts.items():
+        if key < 1:
+            new_value = 0
+        else:
+            new_value = round((value / 10000) * 100, 2)
+        result[key] = new_value
+    return result
 flip_coin()
