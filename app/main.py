93e019a9b2e36bac029804d31c16103e8fb70166
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-23 13:33:00 +0200

    first commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import random
+
+def flip_coin() -> None:
+    how_many = 0
+    coin = random.randint(0, 1)
+    if coin == 1:
+        how_many += 1
+    print(how_many)
+
+flip_coin()
