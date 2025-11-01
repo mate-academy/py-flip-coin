6c2c3810d809cfbaf3b6d7cd655892e41a588356
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2025-11-01 20:37:14 +0200

    Feat: Implement binomial distribution simulation and visualization.

--- app/main.py
+++ app/main.py
@@ -3,23 +3,19 @@ import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
-    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
-              5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
-    for __ in range(10000):
+    result = {key: 0.0 for key in range(11)}
+    for _ in range(10_000):
         count = 0
-        for _ in range(10):
-            count += random.randint(0, 1)
-        if count == 0:
-            result[count] += 1
-        else:
-            result[count] += 1
+        for attempt in range(10):
+            count += random.choice([0, 1])
+        result[count] += 1
+
     for key, value in result.items():
-        result[key] = round((value * 100 / 10000), 2)
+        result[key] = round((value * 100 / 10_000), 2)
     return result
 
 
-def draw_gaussian_distribution_graph() -> None:
-    dict_of_numbers = flip_coin()
+def draw_gaussian_distribution_graph(dict_of_numbers: dict) -> None:
     x_coord = dict_of_numbers.keys()
     y_coord = dict_of_numbers.values()
 
@@ -31,5 +27,6 @@ def draw_gaussian_distribution_graph() -> None:
 
 
 if __name__ == "__main__":
-    print(flip_coin())
-    draw_gaussian_distribution_graph()
+    simulations = flip_coin()
+    print(simulations)
+    draw_gaussian_distribution_graph(simulations)
