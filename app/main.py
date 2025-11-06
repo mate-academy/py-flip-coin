e4f5465e50d29e840009b76d14a9538c25fb3e17
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-11-06 23:53:26 +0200

    start flip_coin function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
-# write your code here
+import random
 
 
-def some_function() -> None:
-    pass
+def flip_coin() -> None:
+    coin_sides = ["head", "tail"]
+    cases = 10000
+    result_dict = {}
+    for case in range(cases):
+        times = 10
+        #for time in range(times):
+
+
+
+
