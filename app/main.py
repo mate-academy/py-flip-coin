0855c0dcd0ae87ff66001c2ceceddfe65b3e9cc4
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-11-09 15:27:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    result = {i: 0 for i in range(11)}
+    for _ in range(10_000):
+        heads = sum(random.choice([0, 1]) for x in range(10))
+        result[heads] += 1
+
+    for value in result:
+        result[value] = round(result[value] / 10_000 * 100, 2)
+    return result
+
+
+def draw_distribution(data: dict) -> None:
+    plt.plot(list(data.keys()), list(data.values()), marker=".")
+    plt.title("Normal (Gaussian) distribution of coin flips")
+    plt.xlabel("Amount of heads in 10 flips")
+    plt.ylabel("Probability in 10 000 trials, %")
+    plt.show()
+
+
+if __name__ == "__main__":
+    trials = flip_coin()
+    print(trials)
+    draw_distribution(trials)
