38948f024e79902d47fc5193c5de6942f58c2400
Oleksii Shcherbak <qksardas001@mail.com>
2025-11-13 17:07:06 +0200

    py-flip-coin-solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import random
+from typing import Dict
+
+
+def flip_coin() -> Dict[int, float]:
+    cases = 10000
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(cases):
+        heads = 0
+        for _ in range(10):
+            if random.randint(0, 1) == 1:
+                heads += 1
+        results[heads] += 1
+
+    for i in results:
+        results[i] = round(results[i] / cases * 100, 2)
+
+    return results
