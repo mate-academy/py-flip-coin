067d61e22f1c38b645f40074cb13ade613f3d8df
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-11-18 18:50:51 -0300

    New Solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import random
 
 
 def flip_coin() -> dict:
+
     total_experiments = 10000
     results = {i: 0 for i in range(11)}
-
 
     for _ in range(total_experiments):
         quantidade_de_caras = 0
