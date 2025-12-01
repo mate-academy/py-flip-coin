ee781c6ef8470cfba7ee139e330d51b82923ace2
Ievgen German <german.kbrza@gmail.com>
2025-12-01 21:49:19 +0200

    solution 1

--- app/main.py
+++ app/main.py
@@ -5,17 +5,16 @@ import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
-    count_heads_dict = {}
-    for number_of_cases in range(1000):
+    count_heads_dict = {keys: 0 for keys in range(11)}
+    number_iterations = 100000
+    for number_of_cases in range(number_iterations):
         count_head = 0
         for number_of_flip in range(10):
             if randint(1, 2) == 1:
                 count_head += 1
-        if count_head in count_heads_dict:
-            count_heads_dict[count_head] += 0.1
-        else:
-            count_heads_dict[count_head] = 0.1
-    return dict(sorted({key: round(value, 2)
+        count_heads_dict[count_head] += 1
+
+    return dict(sorted({key: round(value * 100 / number_iterations, 2)
                         for key, value in
                         count_heads_dict.items()}.items()))
 
