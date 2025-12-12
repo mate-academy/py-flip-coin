315178fe3849bb3fe9d54f6c01e3cd345a01a96e
SSSSSADO <grushkovik.danil@gmail.com>
2025-12-12 20:36:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    cases = 10000
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(cases):
+        heads = 0
+        for _ in range(10):
+            if random.random() < 0.5:
+                heads += 1
+        results[heads] += 1
+
+    for k in results:
+        results[k] = round(results[k] / cases * 100, 2)
+
+    return results
+
+
+print(flip_coin())
