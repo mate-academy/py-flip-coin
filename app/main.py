1b63bc619306a76e1343c54479cf194a8c805eef
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-26 22:05:15 +0100

    implement flip_coin flipping logic

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+def flip_coin() -> dict:
+    result = {}
+    for i in range(10000):
+        ten_flips = 0
+        for _ in range(10):
+            coin_flip = random.randint(0, 1)
+            if coin_flip == 1:
+                ten_flips += 1
+        result[i] = ten_flips / 10
+    print(result)
+    return result
+
+flip_coin()
