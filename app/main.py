9d37983ca3407908f8fa9b5804ce65329e29595c
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-26 23:07:43 +0100

    fix flip_coin

--- app/main.py
+++ app/main.py
@@ -4,15 +4,17 @@ import numpy as np
 
 
 def flip_coin() -> dict:
-    result = {}
-    for i in range(10000):
+    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    for _ in range(10000):
         ten_flips = 0
         for _ in range(10):
             coin_flip = random.randint(0, 1)
             if coin_flip == 1:
                 ten_flips += 1
-        result[i] = ten_flips / 10
-    print(result)
+        result[ten_flips] += 1
+
+    for key, value in result.items():
+        result[key] = round(value / 100, 2)
     return result
 
 def draw_gaussian_distribution_graph() -> None:
