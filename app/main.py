7199de13a99c71dd42f4f99920277599148e009b
Sasha <sasha.ryabokon14@gmail.com>
2026-01-07 20:03:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict[int, float]:
+    result: dict[int, float] = {i: 0 for i in range(11)}
+    trials: int = 10000
+    for _ in range(trials):
+        heads: int = 0
+        for _ in range(10):
+            if random.randint(0, 1) == 1:
+                heads += 1
+        result[heads] += 1
+    for key in result:
+        result[key] = (result[key] / trials) * 100
+    return result
