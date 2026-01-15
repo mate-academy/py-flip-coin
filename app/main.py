1deda87bec8fc7192100d61bd7e18449361ba007
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-15 20:17:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+from collections import Counter
+
+
+def flip_coin() -> dict:
+    count_heads = []
+    for _ in range(10_000):
+        count_heads.append(
+            sum([random.choice([0, 1]) for _ in range(10)])
+        )
+    count_mapping = Counter(count_heads)
+    return {
+        key: round(value / 10_000 * 100, 2)
+        for key, value in count_mapping.items()
+    }
