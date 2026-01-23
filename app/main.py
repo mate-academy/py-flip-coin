5329e45c9571b4788dc8858103502c1eec9b8382
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-23 16:06:24 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from random import randrange
+
+
+def flip_coin() -> dict:
+    dict1 = {i: 0 for i in range(0, 11)}
+    for case in range(10000):
+        heads = 0
+        for _ in range(10):
+            heads += 1 if randrange(2) == 1 else + 0
+        dict1[heads] += 1
+
+    for i2 in range(11):
+        percent = dict1.get(i2) / 10000 * 100
+        rounded_percent = round(percent, 2)
+        dict1[i2] = rounded_percent
+    return dict1
