56e66ac527200630e2c6eb2c88804394669f5093
Danylo <danil.bogomazov1356@gmail.com>
2026-02-13 16:10:42 +0200

    Create flip_coin function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result_dict = {
+        key: 0
+        for key in range(11)
+    }
+    for _ in range(10000):
+        chance = sum(1 for _ in range(10) if random.randint(0, 1) == 1)
+        result_dict[chance] += 1
+    return {
+        key: value / 100
+        for key, value in result_dict.items()
+    }
