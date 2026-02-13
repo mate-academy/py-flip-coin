9d1359589be04c11eb902822f25ee1ae5e0ce6d6
matzxcss <matheuslacerda01@proton.me>
2026-02-13 14:00:23 -0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import random
+
+
+def head_or_tail_counter() -> dict:
+    head = 0
+    tail = 0
+    for _ in range(10):
+        if random.randint(0, 1) == 0:
+            head += 1
+        else:
+            tail += 1
+    percent_results = {"Heads": head / 10, "Tails": tail / 10}
+    return {"Heads": head, "Tails": tail, "Percent Results": percent_results}
+
+
+def flip_coin() -> dict:
+    overall_score = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        result = head_or_tail_counter()
+        num_heads = result["Heads"]
+        overall_score[num_heads] += 1
+    return convert_to_percentage(overall_score)
+
+
+def convert_to_percentage(score: dict) -> dict:
+    total = sum(score.values())
+    round_percents = {
+        key: round(value / total * 100, 2) for key, value in score.items()
+    }
+    return round_percents
