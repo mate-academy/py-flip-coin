e97c494d7b1138b1eb011e093a81ba2159ae4216
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-18 16:36:54 +0100

    „final”

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    counter = 0
+    moneta = ["head", "tail"]
+    lista = []
+    for number in range(0, 10000):
+        lista.append(counter)
+        counter = 0
+        for number in range(0, 10):
+            if random.choice(moneta) == "head":
+                counter += 1
+
+    slownik = dict(zip([number for number in range(0, 11)],
+                       [0 for number in range(0, 11)]))
+    for number in lista:
+        if number in slownik:
+            slownik[number] += 1
+
+    for number in range(0, 11):
+        percentage = (slownik[number] / 10000) * 100
+        slownik[number] = round(float(percentage), 2)
+
+    return slownik
