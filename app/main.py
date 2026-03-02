4e8810bb7cccb08c455eb275df7061f842124fad
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-03-02 13:39:58 +0800

    Add tests

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    total_cases = 10000
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(total_cases):
+        heads_count = 0
+
+        for _ in range(10):
+            if random.choice(["H", "T"]) == "H":
+                heads_count += 1
+
+        results[heads_count] += 1
+
+
+    for key in results:
+        results[key] = round(results[key] / total_cases * 100, 2)
+
+    return results
