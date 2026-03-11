cca804e8b07e52f766abd4fa1e910ff10f7b70d2
Michal Lysakowski <michaslysakowski@gmail.com>
2026-03-11 13:32:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+from typing import Dict
+
+
+def flip_coin(num_experiments: int = 10_000) -> Dict[int, float]:
+    heads_counts: Dict[int, int] = {k: 0 for k in range(11)}
+
+    for _ in range(num_experiments):
+        heads = 0
+        for _ in range(10):
+            if random.randint(0, 1) == 1:
+                heads += 1
+        heads_counts[heads] += 1
+
+    return {
+        heads: (count / num_experiments) * 100
+        for heads, count in heads_counts.items()
+    }
