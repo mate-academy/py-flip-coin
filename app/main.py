01b33622e71ae84ae0e4c308774577476a674a4d
Anton Bublykov <mw.anton2000@gmail.com>
2026-03-11 18:37:03 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import random
+from matplotlib import pyplot as plt
+
+
+def flip_coin() -> dict:
+    option_coin = ["crown", "coin"]
+    result = {}
+    crown = 0
+
+    for i in range(10000):
+        for count in range(10):
+            result_of_tossing_coin = random.choice(option_coin)
+
+            if result_of_tossing_coin == "crown":
+                crown += 1
+
+        result[crown] = result.get(crown, 0) + 1
+        crown = 0
+
+    result = {key: round(100 * (value / 10000), 2)
+              for key, value in sorted(result.items())}
+
+    return result
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    plt.plot(data.keys(), data.values())
+    plt.show()
