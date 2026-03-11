b037277fa7181db9aedd8aa0e7112c2e1cd7e26f
mac <mac@MacBook-Air-mac.local>
2026-03-11 18:16:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    experiments = 100000
+    result = {i: 0 for i in range(11)}
+
+    for _ in range(experiments):
+        heads = 0
+
+        for _ in range(10):
+            if random.choice([0, 1]) == 1:
+                heads += 1
+
+        result[heads] += 1
+
+    for key in result:
+        result[key] = round(result[key] / experiments * 100, 2)
+
+    return result
