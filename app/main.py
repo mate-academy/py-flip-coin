dc8a4396a79c69ec0ce76a812553bb25968d3286
Grimm <grimm60832@gmail.com>
2026-03-21 12:35:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin(n_series: int = 10000) -> dict:  # <- добавь типы
+    counts = {i: 0 for i in range(11)}  # <- пробел после :
+    for _ in range(n_series):
+        heads = 0
+        for _ in range(10):
+            heads += random.choice([0, 1])
+        counts[heads] += 1
+    for k in counts:
+        counts[k] = round(counts[k] / n_series * 100, 2)
+    return counts
