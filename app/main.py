df6af334d116f722c98257ffe4c6efce87763cce
Dmytro Shulha <dimonchik0218@gmail.com>
2026-03-25 19:05:23 +0200

    task flip_coin done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import math
+import random
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def flip_coin(number_of_trials: int = 10000) -> dict[int, float]:
+    dict_result = {i: 0 for i in range(11)}
+    for _ in range(number_of_trials):
+        value = sum(random.randint(0, 1) for _ in range(10))
+        dict_result[value] += 1
+    for key in dict_result:
+        dict_result[key] = round(dict_result[key] / number_of_trials * 100, 2)
+    return dict_result
+
+
+def draw_gaussian_distribution_graph(data: dict[int, float]) -> None:
+    x = list(data.keys())
+    y = list(data.values())
+    plt.bar(x, y, width=0.6, color="C0", alpha=0.7)
+    plt.xlabel("Number of heads"), plt.ylabel("Percentage(%)"), plt.title("Bar_of_pain")
+    n = 10
+    p = 0.5
+    mu = n * p
+    sigma = math.sqrt(n * p * (1 - p))
+    x_smooth = np.linspace(min(x), max(x), 200)
+    gaussian_pdf = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-(x_smooth - mu)**2 / (2 * sigma**2))
+    scale_pdf = gaussian_pdf * 100
+    plt.plot(x_smooth, scale_pdf, color='C1', linewidth=2)
+    plt.xticks(x)
+    plt.legend(['Gaussian approx', 'Simulated'])
+    plt.grid(True, linestyle='--', alpha=0.4)
+    plt.tight_layout()
+    plt.show()
