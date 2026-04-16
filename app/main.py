91f18b6d4956a2258dbaebfceadd9391fb69b3de
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-04-16 21:23:02 +0300

    Fixed flake issues

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ def flip_coin(series: int = 10000) -> dict:
         head_count = attempt.count(1)
         result[head_count] = result.get(head_count, 0) + 1
     return {
-        key:round(value / series * 100, 2)
+        key: round(value / series * 100, 2)
         for key, value in sorted(result.items())
     }
