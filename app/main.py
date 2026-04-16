b8b70e8a9a407df59989b110bdf6c9d9279bb7b7
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-04-16 21:11:32 +0300

    Implemented flip_coin function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from random import choices
+
+
+def flip_coin(series: int = 10000) -> dict:
+    result = {}
+    for num in range(series):
+        attempt = choices([0, 1], k=10)
+        head_count = attempt.count(1)
+        result[head_count] = result.get(head_count, 0) + 1
+    return {
+        key:round(value / series * 100, 2)
+        for key, value in sorted(result.items())
+    }
