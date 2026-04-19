0004f6a43e710233b67c0db5217acbd642c81a88
andriihrechko <andriihrechko250703@gmail.com>
2026-04-19 18:42:11 +0300

    feat: implement flip_coin() and draw_gaussian_distribution_graph() features

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    head_dict = {i: 0 for i in range(0, 11)}
+    count = 10000
+    for _ in range(count):
+        current_coin_list = [
+            random.choice(("head", "tail"))
+            for _ in range(10)
+        ]
+        head_count = current_coin_list.count("head")
+        head_dict[head_count] += 1
+
+    for key, value in head_dict.items():
+        head_dict[key] = round(value / count * 100, 2)
+
+    return head_dict
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    plt.plot(data.keys(), data.values())
+    plt.ylim(0, 100)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.yticks(range(0, 101, 10))
+    plt.xticks(range(0, 11, 1))
+    plt.show()
+
+
+draw_gaussian_distribution_graph(flip_coin())
