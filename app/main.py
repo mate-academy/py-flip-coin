ae22fdff6cd538b515e02f50654d0cfa9006ff18
Alisa Hoys <hoys@protonmail.com>
2026-04-19 10:09:18 +0200

    Wrote flip_coin function. tests passed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    run = 0
+    result = {i : 0 for i in range(11)}
+    total_runs = 10000
+
+    while run < total_runs:
+        heads_count = 0
+        for i in range(10):
+            random_number = random.randint(0, 1)
+            if random_number == 0:
+                heads_count += 1
+        result[heads_count] += 1
+        run += 1
+
+    for key in result:
+        result[key] = result[key] / total_runs * 100
+
+    return result
