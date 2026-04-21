7410e7f51548e54665f3fd4185fc0cc1be3c1f0c
Vijay Chand <vijaychand6979@googlemail.com>
2026-04-21 12:18:37 +0100

    added code

--- app/main.py
+++ app/main.py
@@ -3,24 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-no_of_rounds = 10000
-no_of_flips = 10
-
-
-def flip_coin() -> dict:
+def flip_coin(total_rounds: int = 10000, total_flips: int = 10) -> dict:
     # setup empty results dictionary
     results = {}
 
-    # init results 0 to no_of_flips (inclusive) heads to 0
-    for i in range(no_of_flips + 1):
+    # init results 0 to total_flips (inclusive) heads to 0
+    for i in range(total_flips + 1):
         results[i] = 0
 
-    # repeat tests no_of_rounds times
-    for _ in range(no_of_rounds):
-        # start round of no_of_flips flips
+    # repeat tests total_rounds times
+    for _ in range(total_rounds):
+        # start round of total_flips flips
         # record number of heads, 0 = head, 1 = tail
         count_heads = 0
-        for _ in range(no_of_flips):
+        for _ in range(total_flips):
             if random.randint(0, 1) == 0:
                 count_heads += 1
 
@@ -28,13 +24,15 @@ def flip_coin() -> dict:
         results[count_heads] += 1
 
     # calculate results as percentages
-    for i in range(no_of_flips + 1):
+    for i in range(total_flips + 1):
         results[i] = round(results[i] / 10000 * 100, 2)
 
     return results
 
 
-def draw_gaussian_distribution_graph(heads_count: dict) -> None:
+def draw_gaussian_distribution_graph(
+    heads_count: dict, total_flips: int = 10
+) -> None:
     x_values = heads_count.keys()
     y_values = heads_count.values()
 
@@ -45,7 +43,7 @@ def draw_gaussian_distribution_graph(heads_count: dict) -> None:
     plt.title("Gaussian distribution")
     plt.ylim(0, 100)
     plt.yticks(np.arange(0, 101, 10))
-    plt.xlim(0, no_of_flips)
-    plt.xticks(np.arange(0, no_of_flips + 1, 1))
+    plt.xlim(0, total_flips)
+    plt.xticks(np.arange(0, total_flips + 1, 1))
 
     plt.show()
