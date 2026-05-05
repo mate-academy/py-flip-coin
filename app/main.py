898224afc5861bb8769f2ea6b32fe3f02c1095ca
Paweł Florczyk <pawelf775@gmail.com>
2026-05-05 13:42:50 +0200

    Solve

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    gauss = {i: 0.0 for i in range(11)}
+
+    for case in range(10000):
+        flips = random.choices([0, 1], k=10)
+        head = sum(flips)
+        gauss[head] += 1
+
+    for key in gauss:
+        gauss[key] = round(gauss[key] / 10000 * 100, 2)
+
+    return gauss
