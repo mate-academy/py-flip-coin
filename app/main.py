7f828d0ba33a30cbee2bbab5483f661956f77999
Shongi <shongi85@gmail.com>
2026-05-14 16:26:37 +0200

    coint flip random

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict[int, float]:
+    trials = 10000
+    flips = 10
+
+    head_counts = {i: 0 for i in range(11)}
+
+    for _ in range(trials):
+        heads = sum(random.randint(0, 1) for _ in range(flips))
+        head_counts[heads] += 1
+
+    percentages = {
+        head_number: round((head_count / trials) * 100, 2)
+        for head_number, head_count in head_counts.items()
+    }
+
+    return percentages
+
+
+def draw_gaussian_distribution_graph() -> None:
+    percentages = flip_coin()
+
+    head_numbers = list(percentages.keys())
+    percentages_values = list(percentages.values())
+
+    plt.plot(head_numbers, percentages_values)
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.title("Gaussian distribution")
+    plt.show()
