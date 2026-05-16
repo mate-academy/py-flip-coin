02d30888e784695cee7f62114d545db18dafd5ac
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-05-16 16:33:36 +0300

    feat: added 'flip_coin' func and 'draw_gaussian_distribution_graph' func

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin(cases: int = 10000) -> dict:
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(cases):
+        heads = 0
+
+        for _ in range(10):
+            if random.choice(["heads", "tails"]) == "heads":
+                heads += 1
+
+        results[heads] += 1
+
+    return {
+        heads: round(count / cases * 100, 2)
+        for heads, count in results.items()
+    }
+
+
+def draw_gaussian_distribution_graph() -> None:
+    distribution = flip_coin()
+
+    x_coordinate = list(distribution.keys())
+    y_coordinate = list(distribution.values())
+
+    plt.plot(x_coordinate, y_coordinate, marker="o")
+    plt.xlabel("Number of heads")
+    plt.ylabel("Percentage")
+    plt.title("Gaussian Distribution of Coin Flips")
+    plt.grid(True)
+
+    plt.show()
