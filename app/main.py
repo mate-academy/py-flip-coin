34924ce887ef8a099516047a23642ad982bff8ab
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-27 17:19:39 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 import random
 import matplotlib.pyplot as plt
 
+
 def flip_coin() -> dict:
-    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    result = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+
+
     for i in range(10000):
         current_eagles = 0
         for _ in range(10):
