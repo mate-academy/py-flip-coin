97359e809b03e9a2177fb2f218c5b8d1278ae3ad
ToJaVadym <vadymivaneiko2@gmail.com>
2026-05-28 14:45:11 +0200

    Deleted blank line and added annotation

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import random
 
 
-def flip_coin():
+def flip_coin() -> dict:
     heads_count = 0
-    results = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    results = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+               6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
     for _ in range(10000):
         heads_count = 0
         for _ in range(10):
@@ -14,4 +15,3 @@ def flip_coin():
     for key in results:
         results[key] = results[key] / 10000 * 100
     return results
-
