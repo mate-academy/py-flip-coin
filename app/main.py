8b2e9cf6a4dcbf54e9612f5cbaec0f609967620f
Teashrock <kajitsu22@gmail.com>
2026-07-13 23:44:23 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import random
 def flip_coin() -> dict[int, float]:
     iterations = 10000
     result: dict[int, float] = {}
-    score: dict[int, int] = {iteration: 0 for iteration in range(11)}
+    score: dict[int, int] = {iteration: 0 for iteration in range(iterations)}
     for iteration in range(iterations):
         for _ in range(10):
             score[iteration] += random.choice([0, 1])
