be9a99a1c8bb502dd329000e0dcf6781c913ad8c
Teashrock <kajitsu22@gmail.com>
2026-07-14 18:16:38 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def flip_coin() -> dict[int, float]:
     iterations = 10000
     result: dict[int, float] = {}
     score: dict[int, int] = {iteration: 0 for iteration in range(11)}
-    heads_count = 0
     for iteration in range(iterations):
+        heads_count = 0
         for _ in range(10):
             if random.choice([0, 1]) == 1:
                 heads_count += 1
