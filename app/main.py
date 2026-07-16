ad836a58024d1a9ee9a3d457e3f21656009343b3
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-07-16 22:14:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import random
+
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+
+def flip_coin(num_of_flips: int = 10,
+              num_of_cases: int = 10000) -> dict:
+    heads_count = {number: 0 for number in range(num_of_flips + 1)}
+
+    for _ in range(num_of_cases):
+        heads = sum(random.randint(0, 1) for _ in range(num_of_flips))
+        heads_count[heads] += 1
+
+    return {
+        heads: round(count / num_of_cases * 100, 2)
+        for heads, count in heads_count.items()
+    }
+
+
+def draw_gaussian_distribution_graph() -> None:
+    distribution = flip_coin()
+
+    plt.plot(list(distribution.keys()), list(distribution.values()))
+    plt.title("Gaussian Distribution of Coin Flips")
+    plt.xlabel("Number of heads")
+    plt.ylabel("Percentage of cases, %")
+    plt.show()
